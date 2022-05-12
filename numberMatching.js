641398899bc3934656d62bfbee266d83c1d7f265
Anatoli Nachev <36029469+CodingPawn@users.noreply.github.com>
2022-05-12 21:08:31 +0300

    numberMatching.js
    This is a simple number matching game.

---
+++ numberMatching.js
@@ -0,0 +1,14 @@
+function numberMatching(input) {
+    let randomNumber = Math.floor(Math.random() * 10 + 1);
+    let number = Number(input[0]);
+
+    if (randomNumber == number) {
+        console.log(randomNumber);
+        console.log("The number matches!");
+    } else {
+        console.log(randomNumber);
+        console.log("The number doesn't match!")
+    }
+};
+
+numberMatching([5]);
